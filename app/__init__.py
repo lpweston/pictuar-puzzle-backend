40afc59759444ecd9964b09f9af44eb23c09e3b0
lpweston <l.weston@outlook.com>
2019-10-03 15:30:32 +0100

    config name

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ db = SQLAlchemy()
 def create_app(config_name):
     from app.models import Image
     app = FlaskAPI(__name__, instance_relative_config=True)
+    print(config_name)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
