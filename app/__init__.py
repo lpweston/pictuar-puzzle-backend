1765c855231d40adc1c6a198152386d7235df676
lpweston <l.weston@outlook.com>
2019-10-04 12:43:55 +0100

    cors 5

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ db = SQLAlchemy()
 def create_app(config_name):
     from app.models import Image, PieceBeginner
     app = FlaskAPI(__name__, instance_relative_config=True)
-    CORS(app, support_credentials=True)
+    CORS(app, supports_credentials=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
