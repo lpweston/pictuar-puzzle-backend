c7025c0b8e1f51429bbe6bcd35941aa0efaf2c5a
lpweston <l.weston@outlook.com>
2019-10-04 11:21:53 +0100

    cors troubleshooting 3

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import os
-from flask_cors import CORS, cross_origin
 
 from app import create_app
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
- CORS(app)
 
 if __name__ == '__main__':
     app.run()
