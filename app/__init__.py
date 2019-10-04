18c8ec3bec3f7578fd6f45902e3c3c4eb1eea7bd
lpweston <l.weston@outlook.com>
2019-10-04 11:27:39 +0100

    cors troubleshooting 5

--- app/__init__.py
+++ app/__init__.py
@@ -13,13 +13,13 @@ db = SQLAlchemy()
 def create_app(config_name):
     from app.models import Image, PieceBeginner
     app = FlaskAPI(__name__, instance_relative_config=True)
-    CORS(app)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     #app.config['CORS_HEADERS'] = 'Content-Type'
     db.init_app(app)
 
+    CORS(app)
     @app.route('/')
     def hello():
         return 'hello welcome to my api'
