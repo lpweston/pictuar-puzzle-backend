37797f2e82c8900d7f519e7e831ec757af6f0e80
lpweston <l.weston@outlook.com>
2019-10-04 11:18:55 +0100

    cors troubleshooting 2

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,6 @@ db = SQLAlchemy()
 def create_app(config_name):
     from app.models import Image, PieceBeginner
     app = FlaskAPI(__name__, instance_relative_config=True)
-    CORS(app)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
