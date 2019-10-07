d4ccc9612d8f7f819ca63631352dfb76fe9518ba
lpweston <l.weston@outlook.com>
2019-10-07 16:47:12 +0100

    refactored games endpoint

--- utils/randomiser.py
+++ utils/randomiser.py
@@ -10,50 +10,63 @@ import base64
 def randomiser(diff):
     assignment = {}
     images = {}
-    req = requests.get('https://pictuar-puzzle.herokuapp.com/tiles')
-    if req.status_code==200:
-        tiles = json.loads(req.content.decode('utf-8'))
-        whole = Image.new('RGBA',(400,400),'white')
-        random.shuffle(tiles)
+    # req = requests.get('https://pictuar-puzzle.herokuapp.com/tiles')
+    # if req.status_code==200:
+    #     tiles = json.loads(req.content.decode('utf-8'))
+    tiles = [{
+      "id": 2,
+      "url": "https://i.imgur.com/RRU1Nl1.png"
+    },{
+      "id": 1,
+      "url": "https://i.imgur.com/RRU1Nl1.png"
+    },{
+      "id": 3,
+      "url": "https://i.imgur.com/RRU1Nl1.png"
+    },{
+      "id": 4,
+      "url": "https://i.imgur.com/RRU1Nl1.png"
+    }]
+    whole = Image.new('RGBA',(400,400),'white')
+    random.shuffle(tiles)
 
-        for num in range(0,diff):
-                tile = tiles[num]
-                url = tile.get('url', '')
-                imgid = tile.get('id','')
-                assignment[num] = imgid
-                buffer = tempfile.SpooledTemporaryFile(max_size=1e9)
-                r = requests.get(url, stream=True)
-                if r.status_code == 200:
-                    downloaded = 0
-                    filesize = int(r.headers['content-length'])
-                    for chunk in r.iter_content():
-                        downloaded += len(chunk)
-                        buffer.write(chunk)
-                    buffer.seek(0)
-                    images[num] = Image.open(io.BytesIO(buffer.read()))
-                buffer.close()
-       
-        whole.paste(images[0].resize((200,200)),(0,0))
-        whole.paste(images[1].resize((200,200)),(200,0))
-        whole.paste(images[2].resize((200,200)),(0,200))
-        whole.paste(images[3].resize((200,200)),(200,200))
+    for num in range(0,diff):
+            tile = tiles[num]
+            url = tile.get('url', '')
+            imgid = tile.get('id','')
+            assignment[num] = imgid
+            buffer = tempfile.SpooledTemporaryFile(max_size=1e9)
+            r = requests.get(url, stream=True)
+            if r.status_code == 200:
+                downloaded = 0
+                filesize = int(r.headers['content-length'])
+                for chunk in r.iter_content():
+                    downloaded += len(chunk)
+                    buffer.write(chunk)
+                buffer.seek(0)
+                images[num] = Image.open(io.BytesIO(buffer.read()))
+            buffer.close()
+    
+    whole.paste(images[0].resize((200,200)),(0,0))
+    whole.paste(images[1].resize((200,200)),(200,0))
+    whole.paste(images[2].resize((200,200)),(0,200))
+    whole.paste(images[3].resize((200,200)),(200,200))
 
-        buffered = io.BytesIO()
-        whole.save(buffered, format="PNG")
-        img_str = base64.b64encode(buffered.getvalue())
+    buffered = io.BytesIO()
+    whole.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue())
 
-        imgururl = 'https://api.imgur.com/3/image'
-        payload = {'image': img_str}
-        files = {}
-        headers = {
-        'Authorization': 'Bearer fc0a9f7020eae6353ae08011ef2852caff0e0922'
-        }
-        response = requests.request('POST', url='https://api.imgur.com/3/image', headers = headers, data = payload, files = files, allow_redirects=False)
-        if response.status_code==200:
-            res = json.loads(response.content.decode('utf-8'))
-            assignment['url'] = res['data']['link']
+    imgururl = 'https://api.imgur.com/3/image'
+    payload = {'image': img_str}
+    files = {}
+    headers = {
+    'Authorization': 'Bearer fc0a9f7020eae6353ae08011ef2852caff0e0922'
+    }
+    response = requests.request('POST', url='https://api.imgur.com/3/image', headers = headers, data = payload, files = files, allow_redirects=False)
+    if response.status_code==200:
+        res = json.loads(response.content.decode('utf-8'))
+        assignment['url'] = res['data']['link']
 
-        return assignment
+    return assignment
 
 
 randomiser(4)
